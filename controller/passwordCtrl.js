314c475d852dab1a6c817df66c5ce1c48cb5b1a3
casanova8172 <leelanandkatihar01@gmail.com>
2026-01-11 14:06:54 +0530

    Sending Reset Password Link,Update forgot db using uuid

--- controller/passwordCtrl.js
+++ controller/passwordCtrl.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const Forgotpassword = require("../models/forgotPass");
 const uuid = require("uuid");
 const sib = require("sib-api-v3-sdk");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 const ForgotpasswordCont = async (req, res, next) => {
@@ -11,12 +12,15 @@ const ForgotpasswordCont = async (req, res, next) => {
         const user = await User.findOne({ where: { email } });
 
         if (!user) {
-            return res.status(404).json({ success: false, message: "User with this email does not exist" });
+            return res.status(404).json({
+                success: false,
+                message: "User with this email does not exist",
+            });
         }
 
         //Create a unique ID for the reset link
         const id = uuid.v4();
-
+        console.log(id);
         // Create the record in the Forgotpassword table (Awaited)
         // This assumes a HasMany/BelongsTo relationship is defined in your models
         await user.createForgotpassword({ id, active: true });
@@ -33,7 +37,7 @@ const ForgotpasswordCont = async (req, res, next) => {
             name: "Leelanand",
         };
 
-        const receivers = [{ email: "211220030@nitdelhi.ac.in" }];
+        const receivers = [{ email: email }];
 
         // 5. Send the Email
         const mailResponse = await tranEmailApi.sendTransacEmail({
@@ -44,92 +48,99 @@ const ForgotpasswordCont = async (req, res, next) => {
             htmlContent: `
                 <p>You requested a password reset.</p>
                 <p>Click on the link below to reset your password:</p>
-                <a href="http://localhost:4000/password/resetpassword/${id}">Reset password</a>
+                <a href="http://localhost:4000/pass/reset/${id}">Reset password</a>
             `,
         });
 
-        return res.status(202).json({ 
-            success: true, 
-            message: "Password reset link sent to your email." 
+        return res.status(202).json({
+            success: true,
+            message: "Password reset link sent to your email.",
         });
-
     } catch (err) {
         console.error("Forgot Password Error:", err);
-        return res.status(500).json({ 
-            success: false, 
-            message: "Internal Server Error", 
-            error: err.message 
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: err.message,
         });
     }
 };
 
 
+// Reset Password Controller
+const resetPassword = async (req, res, next) => {
+    console.log('Inside reset password');
+    let id = req.params.id;
 
-// exports.resetPassword = async (req, res, next) => {
-//   console.log("into reset");
-//   let id = req.params.id;
+    try {
+        let forgotpasswordrequest = await Forgotpassword.findOne({ where: { id } })
+        if (!forgotpasswordrequest) {
+            return res.status(404).json('User doesnt exist');
+        }
 
-//   try {
-//     let forgotpasswordrequest = await Forgotpassword.findOne({ where: { id } });
-//     if (!forgotpasswordrequest) {
-//       return res.status(404).json("User doesnt exist");
-//     }
-//     forgotpasswordrequest.update({ active: false });
-
-//     res.status(200).send(`<html>
-//                                     <script>
-//                                         function formsubmitted(e){
-//                                             e.preventDefault();
-//                                             console.log('called')
-//                                         }
-//                                     </script>
-//                                     <form action="/pass/update/${id}" method="POST">
-//                                         <label for="newpassword">Enter New password</label>
-//                                         <input name="newpassword" type="password" required></input>
-//                                         <button>reset password</button>
-//                                     </form>
-//                                 </html>`);
-//     res.end();
-//   } catch (err) {
-//     return res.status(500).json({ message: err });
-//   }
-// };
+        res.status(200).send(`<html>
+                                    <form action="/pass/update/${id}" method="POST">
+                                        <label for="newpassword">Enter New password</label>
+                                        <input name="newpassword" type="password" required></input>
+                                        <button>reset password</button>
+                                    </form>
+                                    <script>
+                                        function formsubmitted(e){
+                                            e.preventDefault();
+                                            console.log('called')
+                                        }
+                                    </script>
+                                </html>`
+        )
+        res.end();
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+}
 
 
+// Update Password Controller
+const updatePassword = async (req, res) => {
+    console.log('Inside update');
 
-// exports.updatePassword = async (req, res, next) => {
-//   console.log("into update");
-//   const { newpassword } = req.query;
-//   const id = req.params.resetpasswordid;
+    try {
+        const { newpassword } = req.body;   
+        const id = req.params.resetpassid;
 
-//   // const token = localStorage.getItem('token')
+        if (!newpassword) {
+            return res.status(400).send("Password is missing");
+        }
 
-//   console.log(typeof newpassword);
-//   try {
-//     const resetpasswordrequest = await Forgotpassword.findOne({
-//       where: { id },
-//     });
-//     const user = await User.findOne({
-//       where: { id: resetpasswordrequest.userId },
-//     });
-//     if (!user) {
-//       return res.status(404).json({ error: "No user Exists", success: false });
-//     }
+        const resetRequest = await Forgotpassword.findOne({ where: { id } });
 
-//     const saltRounds = 10;
-//     bcrypt.hash(newpassword, saltRounds, async (err, hash) => {
-//       if (err) {
-//         throw new Error(err);
-//       }
-//       await user.update({ password: hash });
-//       res.status(201).json({ message: "Successfuly update the new password" });
-//     });
-//   } catch (error) {
-//     return res.status(403).json({ error, success: false });
-//   }
-// };
+        if (!resetRequest || !resetRequest.active) {
+            return res.status(400).send("Reset link expired or invalid");
+        }
+
+        const user = await User.findOne({ where: { id: resetRequest.userId } });
+
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        const hash = await bcrypt.hash(newpassword, 10);
+        await user.update({ password: hash });
+
+        await resetRequest.update({ active: false });
+
+        res.send("<h2>Password updated successfully. You can login now.</h2>");
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Something went wrong");
+    }
+};
+
+
 
 
 module.exports = {
-  ForgotpasswordCont,
+    ForgotpasswordCont,
+    resetPassword,
+    updatePassword,
 };
--- routes/password.js
+++ routes/password.js
@@ -1,15 +1,12 @@
-const path = require('path');
-const passwordController = require('../controller/passwordCtrl')
-
-const middlewareAuthentication = require('../middleware/auth')
-
 const express = require('express');
 const router = express.Router();
+const passwordController = require('../controller/passwordCtrl');
 
-//router.get('/reset/:id',passwordController.resetPassword)
 
-//router.get('/update/:resetpassid',passwordController.updatePassword)
+router.get('/reset/:id', passwordController.resetPassword);
 
-router.use('/forgot',passwordController.ForgotpasswordCont)
+router.post('/update/:resetpassid', passwordController.updatePassword);
+
+router.use('/forgot', passwordController.ForgotpasswordCont);
 
 module.exports = router;
